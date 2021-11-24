ae40370aa02a72d1c0644a7e90892b1adb8f3532
pablomarcel <pablomarcel@outlook.com>
2021-11-23 18:23:38 -0800

    finished

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,15 +4,7 @@ from django.urls import path
 
 from blogging.views import stub_view
 
-# from blogging.views import list_view
-
-# from blogging.views import detail_view
-
 from blogging.views import PostListView, PostDetailView
-
-# urlpatterns = [path('', stub_view, name="blog_index"),
-#                path('posts/<int:post_id>/', stub_view, name="blog_detail"),
-#                ]
 
 urlpatterns = [path('', PostListView.as_view(), name="blog_index"),
                path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail"),
